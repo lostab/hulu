b8a170cb589eec3799206bbacd83c679ccfefba7
lostab <lostab@gmail.com>
2016-05-29 18:08:46 +0530

    fix for validation

--- static/js/common.js
+++ static/js/common.js
@@ -39,7 +39,7 @@ $(document).ready(function(){
     });
     $(".container").css({"min-height": "calc(100% - " + ($(".header").height() + 12 + $(".footer").height()) + "px - " + $(".container").css("padding-top") + ")"});
     
-    $(".itemform .submit").click(function{
+    $(".itemform .submit").click(function(){
         if($.trim($(this).closest(".itemform").find("textarea").val()) == "") {
             $(this).closest(".itemform").find("textarea").focus();
             return false;
