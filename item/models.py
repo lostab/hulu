ad996d16cb8996764085ab96cd0ec77158f0b7d6
lostab <lostab@gmail.com>
2016-05-30 21:05:37 +0530

    fix for root item

--- item/models.py
+++ item/models.py
@@ -46,7 +46,7 @@ class Item(models.Model):
     def get_root_items(self):
         rootitems = []
         if self.belong:
-            for belongitem in self.belong.all():
+            for belongitem in self.belong.all().prefetch_related('itemcontent_set'):
                 for rootitem in belongitem.get_root_items():
                     if rootitem not in rootitems:
                         rootitems.append(rootitem)
