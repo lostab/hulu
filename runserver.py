194fdb7d1169326a22312695811f49fb0c770048
lostab <lostab@gmail.com>
2016-06-24 22:15:33 +0800

    fix for app url

---
+++ runserver.py
@@ -0,0 +1,8 @@
+import os
+
+os.system('python manage.py migrate --run-syncdb')
+
+if 'VCAP_APP_PORT' in os.environ:
+    os.system('python manage.py runserver --noreload --nostatic 0.0.0.0:' + os.environ['VCAP_APP_PORT'])
+else:
+    os.system('python manage.py runserver')
--- runserver.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python manage.py migrate --run-syncdb
-python manage.py runserver --noreload --nostatic 0.0.0.0:$VCAP_APP_PORT
