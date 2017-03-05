5feae528602a3dac5243622da17feb62b40ce948
lostab <lostab@gmail.com>
2017-03-05 21:32:38 +0800

    link check fixed

--- runserver.py
+++ runserver.py
@@ -5,6 +5,7 @@ os.system('python manage.py makemigrations')
 os.system('python manage.py migrate')
 
 if 'PORT' in os.environ:
+    os.system('python cron.py &')
     os.system('python manage.py runserver --noreload --nostatic 0.0.0.0:' + os.environ['PORT'])
 else:
     os.system('python manage.py runserver 0.0.0.0:8000')
