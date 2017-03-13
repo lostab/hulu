90671c8d9bfb53cad6c5b7522175f387ff840c7d
lostab <lostab@gmail.com>
2017-03-13 07:31:55 -0500

    fix for item list

--- user/models.py
+++ user/models.py
@@ -22,7 +22,9 @@ def avatar_file(instance, filename):
 
 def get_item_by_user(user, request):
     try:
-        items = Item.objects.select_related('user').filter(user=user).filter(useritemrelationship__isnull=True).filter(Q(belong__isnull=True)).filter(~Q(status__exact='private') | (Q(status__exact='private') & Q(user=request.user))).order_by('-id').prefetch_related('itemcontent_set')
+        items = Item.objects.select_related('user').filter(user=user).filter(useritemrelationship__isnull=True).filter(Q(belong__isnull=True)).order_by('-id').prefetch_related('itemcontent_set')
+        if user.is_authenticated():
+            items = items.filter(~Q(Q(status_exact='private') & ~Q(user=request.user)))
         items = sorted(items, key=lambda item:item.id, reverse=True)
 
         subitems = Item.objects.filter(user=user).filter(useritemrelationship__isnull=True).filter(Q(belong__isnull=False)).order_by('-id').prefetch_related('itemcontent_set')
