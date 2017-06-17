472bf4f27a187695dc6d88732bc4678cb689b57b
lostab <lostab@gmail.com>
2017-06-17 17:43:09 +0800

    Delete cron.py

--- cron.py
+++
@@ -1,9 +0,0 @@
-import os, django
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hulu.settings")
-from django.conf import settings
-django.setup()
-
-from item.views import checklink
-
-checklinks()
